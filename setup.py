2aba20853ea3b87d8bd5845de328c934e843a14d
denilson <denilson020898@gmail.com>
2023-12-25 18:14:03 +0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="test_fib_py",
-    version="0.0.1",
+    version="0.0.2",
     author="Denilson",
     author_email="denilson020898@gmail.com",
     description="Recursive fibonacci",
