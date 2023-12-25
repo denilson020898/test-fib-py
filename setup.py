b5ca051ca945bb9425398e46e3f9d85b7db3d6f4
denilson <denilson020898@gmail.com>
2023-12-25 18:58:41 +0700

    babi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="denilson_fib_py",
-    version="0.0.2",
+    version="0.0.3",
     author="Denilson",
     author_email="denilson020898@gmail.com",
     description="Recursive fibonacci",
