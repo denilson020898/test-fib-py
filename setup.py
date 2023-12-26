19b3ad136027a42b3d284ea36062815120d499a9
denilson <denilson020898@gmail.com>
2023-12-26 14:37:38 +0700

    rust and python setup

--- setup.py
+++ setup.py
@@ -1,40 +1,26 @@
-from setuptools import find_packages, setup
-import pathlib
+#!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-with open(str(pathlib.Path(__file__).parent.absolute()) + "/fib_py/version.py", "r") as fh:
-    version = fh.read().split("=")[1].replace("'", "")
+from setuptools_rust import Binding, RustExtension
+from setuptools import setup
+from setuptools import dist
+dist.Distribution().fetch_build_eggs["setuptools_rust"]
 
 setup(
-    name="denilson_fib_py",
-    version=version,
-    author="Denilson",
-    author_email="denilson020898@gmail.com",
-    description="Recursive fibonacci",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/denilson020898/test-fib-py",
-    install_requires=[
-        "PyYAML>=4.1.2",
-        "dill>=0.2.8",
-    ],
-    packages=find_packages(exclude=("tests",)),
+    name="denilson_fib_rs",
+    version="0.1",
+    rust_extensions=[RustExtension(
+        "./denilson_fib_rs.denilson_fib_rs",
+        path="Cargo.toml",
+        binding=Binding.PyO3)],
+    packages=["denilson_fib_rs"],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
     ],
-    python_requires=">=3",
-    tests_require=["pytest"],
-    entry_points={
-        'console_scripts': [
-            'fib-number = fib_py.cmd.fib_numb:fib_numb',
-        ],
-    },
-    extras_require={
-        "server": ["Flask>1.0.0"]
-    }
+    zip_safe=False,
 )
